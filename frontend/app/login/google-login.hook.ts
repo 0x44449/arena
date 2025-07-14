4f2b777839ab59a379d67387a63eac55175cd068
0x44449 <0x44449@gmail.com>
2025-07-15 01:14:21 +0900

    env 처리

--- frontend/app/login/google-login.hook.ts
+++ frontend/app/login/google-login.hook.ts
@@ -4,14 +4,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 export default function useGoogleLogin() {
   const loginWithGoogle = async () => {
     const firebaseConfig = {
-      apiKey: "AIzaSyA17PmJhus0_6u_P4c0OLcqov15gE-4_rM",
-      // authDomain: "arena-538a6.firebaseapp.com",
-      authDomain: "arena-dev.com",
-      projectId: "arena-538a6",
-      storageBucket: "arena-538a6.firebasestorage.app",
-      messagingSenderId: "665740865863",
-      appId: "1:665740865863:web:79f440db4cfca634bd9c62",
-      measurementId: "G-W5NNJJ2FGG"
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+      measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
     };
 
     const app = initializeApp(firebaseConfig);
